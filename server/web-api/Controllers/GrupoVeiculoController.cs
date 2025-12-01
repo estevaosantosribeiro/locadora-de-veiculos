1449af2b5c5114fc425d4424900dd494de823022
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-12-01 18:46:09 -0300

    :sparkles: feat: implementar método de inserir grupos de veículos

---
+++ server/web-api/Controllers/GrupoVeiculoController.cs
@@ -0,0 +1,22 @@
+﻿using LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculo.Commands.Inserir;
+using LocadoraDeVeiculos.WebApi.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeVeiculos.WebApi.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/grupo-veiculo")]
+public class GrupoVeiculoController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(InserirGrupoVeiculoResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Inserir(InserirGrupoVeiculoRequest request)
+    {
+        var resultado = await mediator.Send(request);
+
+        return resultado.ToHttpResponse();
+    }
+}
