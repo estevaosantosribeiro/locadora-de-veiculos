b572492e9fbfda72c6adbcc5dff44c09b9a0a8ca
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-12-05 22:20:16 -0300

    :test_tube: test: adicionar teste de integração de cadastro de funcionarios

---
+++ server/LocadoraDeVeiculos.Testes.Integracao/Compartilhado/LocadoraDeVeiculosDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace LocadoraDeVeiculos.Testes.Integracao.Compartilhado;
+
+public static class LocadoraDeVeiculosDbContextFactory
+{
+    public static LocadoraDeVeiculosDbContext CriarDbContext()
+    {
+        var configuracao = CriarConfiguracao();
+
+        var connectionString = configuracao["SQL_CONNECTION_STRING"];
+
+        var options = new DbContextOptionsBuilder<LocadoraDeVeiculosDbContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+        var dbContext = new LocadoraDeVeiculosDbContext(options);
+
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+
+        return dbContext;
+    }
+
+    private static IConfiguration CriarConfiguracao()
+    {
+        var assembly = typeof(LocadoraDeVeiculosDbContextFactory).Assembly;
+
+        return new ConfigurationBuilder()
+            .AddUserSecrets(assembly)
+            .Build();
+    }
+}
---
+++ server/LocadoraDeVeiculos.Testes.Integracao/ModuloFuncionario/RepositorioFuncionarioEmOrmTests.cs
@@ -0,0 +1,42 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+using LocadoraDeVeiculos.Testes.Integracao.Compartilhado;
+
+namespace LocadoraDeVeiculos.Testes.Integracao.ModuloFuncionario;
+
+[TestClass]
+[TestCategory("Testes de Integração - Módulo de Funcionários")]
+public sealed class RepositorioFuncionarioEmOrmTests
+{
+    private LocadoraDeVeiculosDbContext dbContext;
+    private RepositorioFuncionarioEmOrm repositorioFuncionario;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        dbContext = LocadoraDeVeiculosDbContextFactory.CriarDbContext();
+
+        repositorioFuncionario = new RepositorioFuncionarioEmOrm(dbContext);
+    }
+
+    [TestMethod]
+    public async Task Deve_Cadastrar_Funcionario_Corretamente()
+    {
+        // Arrange
+        var funcionario = new Funcionario(
+            "Tiago Rech da Silva",
+            4500,
+            new DateTime(2001, 12, 1)
+        );
+
+        // Act
+        await repositorioFuncionario.InserirAsync(funcionario);
+        dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = await repositorioFuncionario.SelecionarPorIdAsync(funcionario.Id);
+
+        Assert.AreEqual(funcionario, registroSelecionado);
+    }
+}
